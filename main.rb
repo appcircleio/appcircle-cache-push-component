e4cad1b0b3e36e0f48c02601c300d6fdba7e0635
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-02-22 16:43:52 +0300

    Update cache push component
    - Adopt dependencies section to macOS agent
    - Change nonzero exit conditions with exit=0 and @@[error]
    - Add md5sum check before zip file upload to remote storage
    - Add zip logs to pipeline artifacts (reachable from UI)
    - Ignore . and .. on file/folder globbing (subfolders included)
    - Expand exclude pattern for zip excludes on some cases
    - Add docs for include and exclude patterns

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'English'
 require 'net/http'
 require 'json'
+require 'os'
+require 'digest'
 
 def get_env_variable(key)
   return nil if ENV[key].nil? || ENV[key].strip.empty?
@@ -9,31 +11,41 @@ def get_env_variable(key)
 end
 
 def run_command(command)
+  unless system(command)
+    puts "Unexpected exit with code #{$CHILD_STATUS.exitstatus}. Check logs for details."
+    exit 0
+  end
+end
+
+def run_command_with_log(command)
   puts "@@[command] #{command}"
-  exit $CHILD_STATUS.exitstatus unless system(command)
+  run_command(command)
 end
 
-def run_command_silent(command)
-  exit $CHILD_STATUS.exitstatus unless system(command)
+def abort_with0(message)
+  puts "@@[error] #{message}"
+  exit 0
 end
 
-def install_deps_if_not_exist(tool)
-  run_command_silent("dpkg -s #{tool} > /dev/null 2>&1 || apt-get -y install #{tool} > /dev/null 2>&1")
-end
-
-ac_cache_included_paths = get_env_variable('AC_CACHE_INCLUDED_PATHS') || abort('Included paths must be defined.')
-ac_cache_excluded_paths = get_env_variable('AC_CACHE_EXCLUDED_PATHS')
+ac_cache_included_paths = get_env_variable('AC_CACHE_INCLUDED_PATHS') || abort_with0('Included paths must be defined.')
+ac_cache_excluded_paths = get_env_variable('AC_CACHE_EXCLUDED_PATHS') || ''
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR')
-ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort('Cache label path must be defined.')
+ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort_with0('Cache label path must be defined.')
 
-ac_token_id = get_env_variable('AC_TOKEN_ID') || abort('AC_TOKEN_ID env variable must be set when build started.')
+ac_token_id = get_env_variable('AC_TOKEN_ID') || abort_with0('AC_TOKEN_ID env variable must be set when build started.')
 ac_callback_url = get_env_variable('ASPNETCORE_CALLBACK_URL') ||
-                  abort('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+                  abort_with0('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+
+def ac_output_dir
+  out_dir = get_env_variable('AC_OUTPUT_DIR')
+  out_dir && Dir.exist?(out_dir) ? out_dir : nil
+end
 
 signed_url_api = "#{ac_callback_url}?action=getCacheUrls"
 
-install_deps_if_not_exist('curl')
-install_deps_if_not_exist('zip')
+# check dependencies
+run_command('zip -v |head -2')
+run_command('curl --version |head -1')
 
 @cache = "ac_cache/#{ac_cache_label}"
 zipped = "ac_cache/#{ac_cache_label.gsub('/', '_')}.zip"
@@ -46,6 +58,17 @@ puts ac_repository_path
 puts '------'
 
 system("mkdir -p #{@cache}/repository")
+
+def expand_exclude(pattern)
+  if pattern.end_with?('/\*')
+    exclude = "#{pattern.delete_suffix!('/\*')}/*"
+    exclude = "\"#{exclude}\""
+    exclude += " \"#{pattern.gsub('**/', '')}/*\"" if pattern.include? '**/'
+  else
+    exclude = "\"#{exclude}\""
+  end
+  exclude
+end
 
 def cache_path(base_path, included_path, excluded_paths)
   puts "Global Include: #{included_path}"
@@ -62,10 +85,14 @@ def cache_path(base_path, included_path, excluded_paths)
   end
   zip += ' -x' unless excluded_paths.empty?
   excluded_paths.each do |excluded|
-    zip += " #{excluded}"
+    zip += " #{expand_exclude(excluded)}"
   end
-  run_command("#{zip} > #{zip_file}.log")
-  run_command_silent("ls -lh #{zip_file}")
+  if ac_output_dir
+    system("mkdir -p #{ac_output_dir}/#{@cache}")
+    zip += " > #{ac_output_dir}/#{zip_file}.log"
+  end
+  run_command_with_log(zip)
+  run_command("ls -lh #{zip_file}")
 end
 
 def cache_repository_path(base_path, included_path, excluded_paths)
@@ -83,16 +110,31 @@ def cache_repository_path(base_path, included_path, excluded_paths)
   zip_file = "#{cwd}/#{@cache}/repository/#{included_path.gsub('/', '_')}.zip"
   zip = "zip -r -FS #{zip_file}"
   paths.each do |f|
+    next if ['.', '..'].include?(f)
+    next if f.end_with?('.') || f.end_with?('..')
+
     zip += " #{f}"
   end
   zip += ' -x' unless excluded_paths.empty?
   excluded_paths.each do |excluded|
-    zip += " #{excluded}"
+    zip += " #{expand_exclude(excluded)}"
   end
-  run_command("#{zip} > #{zip_file}.log")
-  run_command_silent("ls -lh #{zip_file}")
+  if ac_output_dir
+    system("mkdir -p #{ac_output_dir}/#{@cache}/repository")
+    zip += " > #{ac_output_dir}/#{@cache}/repository/#{included_path.gsub('/', '_')}.zip.log"
+  end
+  run_command_with_log(zip)
+  run_command("ls -lh #{zip_file}")
 
   Dir.chdir(cwd)
+end
+
+def home
+  if OS.mac?
+    ENV['HOME']
+  else
+    '/setup'
+  end
 end
 
 def get_excluded_paths(paths)
@@ -103,10 +145,9 @@ def get_excluded_paths(paths)
     path = path[1..-1] if !path.empty? && path[0] == '/'
     next if path.empty?
 
-    # @todo: Check $home path for other types of agents and build profiles
     if path.start_with?('~/')
       path = path[('~/'.length)..-1]
-      g_excludes.push("/setup/#{path}")
+      g_excludes.push("#{home}/#{path}")
     else
       r_excludes.push(path)
     end
@@ -121,20 +162,29 @@ ac_cache_included_paths.split(':').each do |included_path|
   included_path = included_path[1..-1] if !included_path.empty? && included_path[0] == '/'
   next if included_path.empty?
 
-  # @todo: Check $home path for other types of agents and build profiles
   if included_path.start_with?('~/')
     included_path = included_path[('~/'.length)..-1]
-    cache_path('/setup', included_path, excluded_paths['global'])
+    cache_path(home, included_path, excluded_paths['global'])
   elsif ac_repository_path
     cache_repository_path(ac_repository_path, included_path, excluded_paths['repository'])
   else
-    puts "Warning: #{included_path} is ignored. It can be used only after Git Clone workflow step."
+    puts "Warning: #{included_path} is skipped. It can be used only after Git Clone workflow step."
   end
 end
 
-run_command_silent("[ -s #{zipped} ] || rm -f #{zipped}")
-run_command("zip -r -0 -FS #{zipped} #{@cache}")
-run_command_silent("ls -lh #{zipped}")
+run_command("[ -s #{zipped} ] || rm -f #{zipped}")
+run_command_with_log("zip -r -0 -FS #{zipped} #{@cache}")
+run_command("ls -lh #{zipped}")
+
+if File.exist?("#{zipped}.md5")
+  pulled_md5sum = File.open("#{zipped}.md5", 'r', &:readline).strip
+  pushed_md5sum = Digest::MD5.file(zipped).hexdigest
+  puts "#{pulled_md5sum} =? #{pushed_md5sum}"
+  if pulled_md5sum == pushed_md5sum
+    puts 'Cache is the same as pulled one. No need to upload.'
+    exit 0
+  end
+end
 
 unless ac_token_id.empty?
   puts ''
@@ -150,6 +200,6 @@ unless ac_token_id.empty?
     puts signed['putUrl']
 
     ENV['AC_CACHE_PUT_URL'] = signed['putUrl']
-    run_command_silent("curl -X PUT -H \"Content-Type: application/zip\" --upload-file #{zipped} $AC_CACHE_PUT_URL")
+    run_command("curl -X PUT -H \"Content-Type: application/zip\" --upload-file #{zipped} $AC_CACHE_PUT_URL")
   end
 end

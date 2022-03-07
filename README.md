b779c9823662b3203ce7aedeac1d73035b059320
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-03-07 12:02:18 +0300

    Update cache push > 1.0.0 (#1)
    - Change nonzero exit conditions with exit=0 and @@[error]
    - Add md5sum check before zip file upload to remote storage
    - Add zip logs to pipeline artifacts (is now reachable from UI)
    - Ignore . and .. on file/folder globbing (also subfolders included)
    - Expand exclude pattern for zip excludes on some cases (fix)
    - Add detailed docs for include and exclude patterns
    - Add @@[error] label to subcommand exit error message
    - Enable use of `/` prefixed system folders
    - Change callback url env variable name
    - Fix curl hang on file upload (with -0 arg)
    - Add duration took to run command with log function
    - Add dynamic system paths to cache push (env vars)

--- README.md
+++ README.md
@@ -5,11 +5,176 @@ Uploads user selected files and folders to Appcircle cache.
 Required Input Variables
 
 - `AC_CACHE_LABEL`: User defined cache label to identify one cache from others. Both cache push and pull steps should have the same value to match.
-- `AC_CACHE_INCLUDED_PATHS`: Specifies the files and folder which should be in cache. Multiple glob patterns can be defined with colon seperated. For example; .gradle:app/build
+- `AC_CACHE_INCLUDED_PATHS`: Specifies the files and folders which should be in cache. Multiple glob patterns can be provided as a colon-separated list. For example; .gradle:app/build
 - `AC_TOKEN_ID`: System generated token used for getting signed url. Zipped cache file is uploaded to signed url.
 - `ASPNETCORE_CALLBACK_URL`: System generated callback url for signed url web service. It's different for various environments.
 
 Optional Input Variables
 
-- `AC_CACHE_EXCLUDED_PATHS`: Specifies the files and folder which should be ignored from cache. Multiple glob patterns can be defined with colon seperated. For example; .gradle/*.lock:*.apk
-- `AC_REPOSITORY_DIR`: Cloned git repository path. Included and excluded paths are defined relative to cloned repository, except `~` prefixed paths.
+- `AC_CACHE_EXCLUDED_PATHS`: Specifies the files and folders which should be ignored from cache. Multiple glob patterns can be provided as a colon-separated list. For example; .gradle/*.lock:*.apk
+- `AC_REPOSITORY_DIR`: Cloned git repository path. Included and excluded paths are defined relative to cloned repository, except `~/`, `/` or environment variable prefixed paths. See following sections for more details.
+
+## Included & Excluded Paths
+
+Cache step uses a pattern in order to select files and folders. That the pattern is not a regexp, it's closer to a shell glob. (_The verb "glob" is an old Unix term for filename matching in a shell._)
+
+Also we have some keywords or characters for special use cases, especially for system folders. Following sections summarize cache step's supported patterns for included and excluded paths.
+
+### System vs. Repository
+
+In order to identify between a repository resource and a system resource, cache step checks prefix for each given pattern.
+
+Resource word, used in the document, means files or folders in this context.
+
+Repository resources begin with directly glob pattern. They shouldn't be prefixed with `/` or other folder tree characters.
+
+For example:
+
+- `.gradle/` is .gradle folder in project repository.
+- `local.properties` is single file in project repository.
+- `*.apk` is related with .apk files in project repository.
+
+Repository resources are generated with git clone on most cases. For this reason, take care of step order while using cache and git clone for repository reources.
+
+On the other hand system resources for `$HOME` begin with `~/` pattern. These resources are generated on build and on most cases they're not included in repository.
+
+For example:
+
+- `~/.gradle/` is .gradle folder at $HOME.
+- `~/Library/Caches/CocoaPods` is Cocoapods caches folder at $HOME.
+
+ Also other system-wide resources are reachable with prefix `/`.
+
+---
+
+**Note:** We should be careful with dynamic folder paths which are temporary on builds.
+
+From build to build, their absolute path is changing. For example, `_appcircle_temp` folder's absolute path is `/Volumes/agent-disk/agent/workflow_data/xjr1walp.ikh/_appcircle_temp` on one build and is `/Volumes/agent-disk/agent/workflow_data/y3jdjln4.0kj/_appcircle_temp` on another build.
+
+So, for those kinds of resources, we should prefix include and exclude paths with Appcircle-specific (reserved) environment variables.
+
+For example:
+
+- `$AC_TEMP_DIR/appcircle_build_ios_simulator/SimulatorDerivedData` is simulator derived data resources at `_appcircle_temp`.
+
+See full list of environment variables at [Appcircle docs](https://docs.appcircle.io/environment-variables/appcircle-specific-environment-variables/).
+
+---
+
+### Glob Patterns
+
+#### `*`
+
+Match zero or more characters. A glob consisting of only the asterisk and no other characters or wildcards will match all files or folders in that folder.
+
+Examples:
+
+- `AC_CACHE_INCLUDED_PATHS=*`: All files and folders in repository included.
+
+We can also focus into subfolders with prefixing parent folders relative to repository.
+
+- `AC_CACHE_INCLUDED_PATHS=app/*`: All files and folders in "app" folder included.
+
+The asterisk is usually combined with a file extension and other characters for prefix or instring matches. See examples below:
+
+- `AC_CACHE_INCLUDED_PATHS=*.properties`: All files with .properties extension in repository root. (_no subfolders included_)
+- `AC_CACHE_INCLUDED_PATHS=gradle*`: All files and folders begin with "gradle" in repository root. (_no subfolders included_)
+- `AC_CACHE_INCLUDED_PATHS=*release*`: All files and folders begin with "gradle" in repository root. (_no subfolders included_)
+
+We can also focus them into subfolders with prefixing parent folders relative to repository.
+
+- `AC_CACHE_INCLUDED_PATHS=app/*.properties`: All files with .properties extension in "app" folder. (_no subfolders included_)
+- `AC_CACHE_INCLUDED_PATHS=app/gradle*`: All files begin with "gradle" in "app" folder. (_no subfolders included_)
+- `AC_CACHE_INCLUDED_PATHS=app/*release*`: All files begin with "gradle" in "app" folder. (_no subfolders included_)
+
+Including subfolders requires recursion. See following section for details of `**` usage.
+
+For the examples above, if you need to exclude relevant folders and select only files, use `/\*` suffixed version of the same pattern in AC_CACHE_EXCLUDED_PATHS. It will discard all matched folders for the related include.
+
+#### `**`
+
+Match all folders recursively. This is used to descend into the folder tree and find all files and folders in subfolders of the current folder.
+
+- `AC_CACHE_INCLUDED_PATHS=**/*.properties`: All files with .properties extension and folders ending with ".properties" in repository.
+- `AC_CACHE_INCLUDED_PATHS=**/gradle*`: All files and folders begin with "gradle" in repository.
+- `AC_CACHE_INCLUDED_PATHS=**/*release*`: All files and folders that contain "release" in repository.
+
+We can also focus into subfolders to make recursion from there with prefixing parent folders relative to repository.
+
+- `AC_CACHE_INCLUDED_PATHS=app/**/*.properties`: All files with .properties extension and folders ending with ".properties" in "app" folder.
+- `AC_CACHE_INCLUDED_PATHS=app/**/gradle*`: All files and folders begin with "gradle" in "app" folder.
+- `AC_CACHE_INCLUDED_PATHS=app/**/*release*`: All files and folders that contain "release" in "app" folder.
+
+For all examples above, if you need to exclude relevant folders and select only files, use `/\*` suffixed version of the same pattern in AC_CACHE_EXCLUDED_PATHS. It will discard all matched folders for the related include.
+
+#### Notice
+
+We should be careful while using excluded paths, especially for cases that defined pattern both has match from file and folder in the same path. Let's explain the situation with an example.
+
+Assume that we want to select all files beginning with `gradle*`.
+
+- `AC_CACHE_INCLUDED_PATHS=**/gradle*`
+
+With above definition we get the following files and folders:
+
+```txt
+**_gradle*.zip:
+  app/gradle/
+  app/gradle/b.txt
+  app/gradle/c.txt
+  app/gradle/a.txt
+  app/gradle.x
+  app/src/gradle.y
+  gradle/
+  gradle/wrapper/
+  gradle/wrapper/gradle-wrapper.properties
+  gradle/wrapper/gradle-wrapper.jar
+  gradle.properties
+  gradlew
+  gradlew.bat
+```
+
+Since we want only files, we basically add below pattern to excludes.
+
+- `AC_CACHE_EXCLUDED_PATHS=**/gradle*/\*`
+
+After the modification, we get the following files:
+
+```txt
+**_gradle*.zip:
+  app/gradle.x
+  app/src/gradle.y
+  gradle.properties
+  gradlew
+  gradlew.bat
+```
+
+But now we have two missing "gradle*" prefixed files under "gradle/wrapper" folder. Our folder exclude removes them from parent folder.
+
+In order to add those missing files, we need to define an additional include pattern for "gradle" subfolder which selects "gradle*" prefixed files like before. Since we want only files, we also define an exclude pattern specific for that subfolder too.
+
+- `AC_CACHE_INCLUDED_PATHS=**/gradle*:gradle/**/gradle*`
+- `AC_CACHE_EXCLUDED_PATHS=**/gradle*/\*:gradle/**/gradle*/\*`
+
+Now we have all "gradle*" prefixed files.
+
+```txt
+**_gradle*.zip:
+  app/gradle.x
+  app/src/gradle.y
+  gradle.properties
+  gradlew
+  gradlew.bat
+
+gradle_**_gradle*.zip:
+  gradle/wrapper/gradle-wrapper.jar
+  gradle/wrapper/gradle-wrapper.properties
+```
+
+As an alternative method, other "gradle*" prefixed files can be added with a specific include pattern like `**/gradle-wrapper.*` without using any extra exclude.
+
+```txt
+**_gradle-wrapper.*.zip
+  gradle/wrapper/gradle-wrapper.jar
+  gradle/wrapper/gradle-wrapper.properties
+```
--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require 'English'
 require 'net/http'
 require 'json'
+require 'os'
+require 'digest'
+require 'set'
 
 def get_env_variable(key)
   return nil if ENV[key].nil? || ENV[key].strip.empty?
@@ -9,67 +12,111 @@ def get_env_variable(key)
 end
 
 def run_command(command)
+  unless system(command)
+    puts "@@[error] Unexpected exit with code #{$CHILD_STATUS.exitstatus}. Check logs for details."
+    exit 0
+  end
+end
+
+def run_command_with_log(command)
   puts "@@[command] #{command}"
-  exit $CHILD_STATUS.exitstatus unless system(command)
+  s = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  run_command(command)
+  e = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  puts "took #{(e - s).round(3)}s"
 end
 
-def run_command_silent(command)
-  exit $CHILD_STATUS.exitstatus unless system(command)
+def abort_with0(message)
+  puts "@@[error] #{message}"
+  exit 0
 end
 
-def install_deps_if_not_exist(tool)
-  run_command_silent("dpkg -s #{tool} > /dev/null 2>&1 || apt-get -y install #{tool} > /dev/null 2>&1")
-end
-
-ac_cache_included_paths = get_env_variable('AC_CACHE_INCLUDED_PATHS') || abort('Included paths must be defined.')
-ac_cache_excluded_paths = get_env_variable('AC_CACHE_EXCLUDED_PATHS')
+ac_cache_included_paths = get_env_variable('AC_CACHE_INCLUDED_PATHS') || abort_with0('Included paths must be defined.')
+ac_cache_excluded_paths = get_env_variable('AC_CACHE_EXCLUDED_PATHS') || ''
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR')
-ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort('Cache label path must be defined.')
+ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort_with0('Cache label path must be defined.')
 
-ac_token_id = get_env_variable('AC_TOKEN_ID') || abort('AC_TOKEN_ID env variable must be set when build started.')
-ac_callback_url = get_env_variable('ASPNETCORE_CALLBACK_URL') ||
-                  abort('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+ac_token_id = get_env_variable('AC_TOKEN_ID') || abort_with0('AC_TOKEN_ID env variable must be set when build started.')
+ac_callback_url = get_env_variable('AC_CALLBACK_URL') ||
+                  abort_with0('AC_CALLBACK_URL env variable must be set when build started.')
+
+def ac_output_dir
+  out_dir = get_env_variable('AC_OUTPUT_DIR')
+  out_dir && Dir.exist?(out_dir) ? out_dir : nil
+end
 
 signed_url_api = "#{ac_callback_url}?action=getCacheUrls"
 
-install_deps_if_not_exist('curl')
-install_deps_if_not_exist('zip')
+# check dependencies
+run_command('zip -v |head -2')
+run_command('curl --version |head -1')
 
 @cache = "ac_cache/#{ac_cache_label}"
 zipped = "ac_cache/#{ac_cache_label.gsub('/', '_')}.zip"
 
-puts 'Inputs:'
+puts '--- Inputs:'
 puts ac_cache_label
 puts ac_cache_included_paths
 puts ac_cache_excluded_paths
 puts ac_repository_path
-puts '------'
+puts '-----------'
 
-system("mkdir -p #{@cache}/repository")
-
-def cache_path(base_path, included_path, excluded_paths)
-  puts "Global Include: #{included_path}"
-
-  glob_pattern = "#{base_path}/#{included_path}"
-
-  paths = Dir.glob(glob_pattern, File::FNM_DOTMATCH)
-  return if paths.empty?
-
-  zip_file = "#{@cache}/#{glob_pattern.gsub('/', '_')}.zip"
-  zip = "zip -r -FS #{zip_file}"
-  paths.each do |f|
-    zip += " #{f}"
-  end
-  zip += ' -x' unless excluded_paths.empty?
-  excluded_paths.each do |excluded|
-    zip += " #{excluded}"
-  end
-  run_command("#{zip} > #{zip_file}.log")
-  run_command_silent("ls -lh #{zip_file}")
+env_dirs = Hash.new('')
+ENV.each_pair do |k, v|
+  env_dirs[v] = k if k.start_with?('AC_') && File.directory?(v)
 end
 
-def cache_repository_path(base_path, included_path, excluded_paths)
-  puts "Repository Include: #{included_path}"
+def expand_exclude(pattern)
+  if pattern.end_with?('/\*')
+    exclude = "#{pattern.delete_suffix!('/\*')}/*"
+    exclude = "\"#{exclude}\""
+    exclude += " \"#{pattern.gsub('**/', '')}/*\"" if pattern.include? '**/'
+  else
+    exclude = "\"#{pattern}\""
+  end
+  exclude
+end
+
+def add_includes(included_paths, zip)
+  included_paths.each do |f|
+    next if ['.', '..'].include?(f)
+    next if f.end_with?('.') || f.end_with?('..')
+
+    zip += " #{f}"
+  end
+  zip
+end
+
+def add_excludes(excluded_paths, zip)
+  return zip if excluded_paths.empty?
+
+  zip += ' -x'
+  excluded_paths.each do |excluded|
+    zip += " #{expand_exclude(excluded)}"
+  end
+  zip
+end
+
+def add_log_file(folder, file, zip)
+  if ac_output_dir
+    system("mkdir -p #{folder}")
+    zip += " > #{folder}/#{file}"
+  end
+  zip
+end
+
+def run_zip(zip_file, zip)
+  run_command_with_log(zip)
+  run_command("ls -lh #{zip_file}")
+end
+
+def cache_path(base_path, included_path, excluded_paths, env_dirs)
+  puts "Include: #{included_path} in #{base_path}"
+
+  unless Dir.exist?(base_path)
+    puts "Warning: #{base_path} doesn't exist yet. Check folder is correct and available."
+    return nil
+  end
 
   cwd = Dir.pwd
   Dir.chdir(base_path)
@@ -77,64 +124,128 @@ def cache_repository_path(base_path, included_path, excluded_paths)
 
   if paths.empty?
     Dir.chdir(cwd)
-    return
+    return nil
   end
 
-  zip_file = "#{cwd}/#{@cache}/repository/#{included_path.gsub('/', '_')}.zip"
+  base_path = "/#{env_dirs[base_path]}" if env_dirs.key?(base_path)
+  zip_file = "#{cwd}/#{@cache}#{base_path}/#{included_path.gsub('/', '_')}.zip"
+  system("mkdir -p #{cwd}/#{@cache}#{base_path}")
   zip = "zip -r -FS #{zip_file}"
-  paths.each do |f|
-    zip += " #{f}"
-  end
-  zip += ' -x' unless excluded_paths.empty?
-  excluded_paths.each do |excluded|
-    zip += " #{excluded}"
-  end
-  run_command("#{zip} > #{zip_file}.log")
-  run_command_silent("ls -lh #{zip_file}")
+  zip = add_includes(paths, zip)
+  zip = add_excludes(excluded_paths, zip)
+  zip = add_log_file("#{ac_output_dir}/#{@cache}#{base_path}", "#{included_path.gsub('/', '_')}.zip.log", zip)
+  run_zip(zip_file, zip)
 
   Dir.chdir(cwd)
+  zip_file
 end
 
-def get_excluded_paths(paths)
-  r_excludes = []
-  g_excludes = []
+def home
+  if OS.mac?
+    ENV['HOME']
+  else
+    '/setup'
+  end
+end
+
+def search_env_dirs(path, env_dirs)
+  return path if env_dirs.key?(path)
+
+  index_of_slash = path.rindex('/')
+  return search_env_dirs(path[0..index_of_slash - 1], env_dirs) if index_of_slash.positive?
+
+  nil
+end
+
+def find_base_path(path, env_dirs)
+  base_path = ''
+  parts = path.split('/')
+  order = 1
+  parts.each do |w|
+    break if order == parts.length
+    break if w.include?('*')
+
+    base_path += "/#{w}" unless w.empty?
+    order += 1
+  end
+  env_path = search_env_dirs(base_path, env_dirs)
+  env_path || base_path
+end
+
+def get_excluded_paths(paths, env_dirs)
+  home = '~/'
+
+  excludes = Hash.new('')
+  excludes[home] = []
+  excludes[''] = [] # repository
 
   paths.split(':').each do |path|
-    path = path[1..-1] if !path.empty? && path[0] == '/'
     next if path.empty?
 
-    # @todo: Check $home path for other types of agents and build profiles
-    if path.start_with?('~/')
-      path = path[('~/'.length)..-1]
-      g_excludes.push("/setup/#{path}")
+    if path.start_with?(home)
+      path = path[(home.length)..-1]
+      excludes[home].push(path)
+    elsif path.start_with?('/')
+      base_path = find_base_path(path, env_dirs)
+      next unless base_path
+
+      excludes[base_path] = [] unless excludes.key?(base_path)
+      excludes[base_path].push(path[(base_path.length + 1)..-1])
     else
-      r_excludes.push(path)
+      excludes[''].push(path)
     end
   end
-  { 'global' => g_excludes, 'repository' => r_excludes }
+  excludes
 end
 
-excluded_paths = get_excluded_paths(ac_cache_excluded_paths)
+excluded_paths = get_excluded_paths(ac_cache_excluded_paths, env_dirs)
 puts excluded_paths
 
+uptodate_zips = Set.new([])
+
 ac_cache_included_paths.split(':').each do |included_path|
-  included_path = included_path[1..-1] if !included_path.empty? && included_path[0] == '/'
   next if included_path.empty?
 
-  # @todo: Check $home path for other types of agents and build profiles
+  zip_file = nil
   if included_path.start_with?('~/')
     included_path = included_path[('~/'.length)..-1]
-    cache_path('/setup', included_path, excluded_paths['global'])
+    zip_file = cache_path(home, included_path, excluded_paths['~/'], env_dirs)
+  elsif included_path.start_with?('/')
+    base_path = find_base_path(included_path, env_dirs)
+    next unless base_path
+
+    zip_file = cache_path(base_path, included_path[(base_path.length + 1)..-1], excluded_paths[base_path], env_dirs)
   elsif ac_repository_path
-    cache_repository_path(ac_repository_path, included_path, excluded_paths['repository'])
+    zip_file = cache_path(ac_repository_path, included_path, excluded_paths[''], env_dirs)
   else
-    puts "Warning: #{included_path} is ignored. It can be used only after Git Clone workflow step."
+    puts "Warning: #{included_path} is skipped. It can be used only after Git Clone workflow step."
   end
+
+  uptodate_zips.add(zip_file.sub("#{Dir.pwd}/", '')) if zip_file
 end
 
-run_command_silent("[ -s #{zipped} ] || rm -f #{zipped}")
-run_command("zip -r -0 -FS #{zipped} #{@cache}")
-run_command_silent("ls -lh #{zipped}")
+# remove dead zips (includes) from pulled zips if not in uptodate set
+Dir.glob("#{@cache}/**/*.zip", File::FNM_DOTMATCH).each do |zip_file|
+  unless uptodate_zips.include?(zip_file)
+    system("rm -f #{zip_file}")
+    puts "Info: #{zip_file} is not in uptodate includes. Removed."
+  end
+end
+system("find #{@cache} -empty -type d -delete")
+
+run_command("[ -s #{zipped} ] || rm -f #{zipped}")
+run_command_with_log("zip -r -0 -FS #{zipped} #{@cache}")
+run_command("ls -lh #{zipped}")
+
+if File.exist?("#{zipped}.md5")
+  pulled_md5sum = File.open("#{zipped}.md5", 'r', &:readline).strip
+  pushed_md5sum = Digest::MD5.file(zipped).hexdigest
+  puts "#{pulled_md5sum} =? #{pushed_md5sum}"
+  if pulled_md5sum == pushed_md5sum
+    puts 'Cache is the same as pulled one. No need to upload.'
+    exit 0
+  end
+end
 
 unless ac_token_id.empty?
   puts ''
@@ -146,10 +257,11 @@ unless ac_token_id.empty?
   response = Net::HTTP.get(uri)
   unless response.empty?
     puts 'Uploading cache...'
-    signed = JSON.parse(response)
-    puts signed['putUrl']
 
+    signed = JSON.parse(response)
     ENV['AC_CACHE_PUT_URL'] = signed['putUrl']
-    run_command_silent("curl -X PUT -H \"Content-Type: application/zip\" --upload-file #{zipped} $AC_CACHE_PUT_URL")
+    puts ENV['AC_CACHE_PUT_URL']
+    curl = 'curl -0 -X PUT -H "Content-Type: application/zip"'
+    run_command_with_log("#{curl} --upload-file #{zipped} $AC_CACHE_PUT_URL")
   end
 end
